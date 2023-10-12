22700247d9460377d2818df0d2393153502e5fa2
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-10-12 08:38:32 -0400

    =tweak not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center gap-1 pb-32 text-lg">
+    <main className="mt-32 flex flex-col items-center justify-center gap-1 pb-32 text-lg">
       <h1>Not Found</h1>
       <p>Count not find requested resource</p>
       <Link href={"/"} className="px-4 py-1 font-medium underline">
