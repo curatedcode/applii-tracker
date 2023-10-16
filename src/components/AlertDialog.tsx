19f22a62b626718a9e9862d5cb70d42f8e449f9d
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-10-15 21:21:00 -0400

    fix dialogs overflowing on mobile

--- src/components/AlertDialog.tsx
+++ src/components/AlertDialog.tsx
@@ -11,7 +11,7 @@ export default function AlertDialog({
 
   return (
     <FocusTrap>
-      <div className="fixed left-0 top-0 z-10 h-screen w-screen backdrop-blur-md">
+      <div className="fixed left-0 top-0 z-10 h-screen w-screen px-4 backdrop-blur-md">
         <div
           className="relative left-1/2 top-1/3 grid w-full max-w-md -translate-x-1/2 -translate-y-1/3 gap-2 rounded-md bg-light-secondary px-5 py-8 text-center text-lg outline-none ring-2 ring-black ring-opacity-20 ring-offset-black focus-within:outline-none focus:outline-none focus-visible:outline-none focus-visible:ring-black focus-visible:ring-offset-black dark:bg-dark-secondary"
           role="alertdialog"
