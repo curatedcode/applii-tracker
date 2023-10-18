630e130eec23a1819d0c29835910e89b8d4178bd
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-10-18 03:17:37 -0400

    make board card text more readable in dark mode

--- src/components/BoardSectionCard.tsx
+++ src/components/BoardSectionCard.tsx
@@ -21,7 +21,7 @@ export default function BoardSectionCard({
   return (
     <Link
       href={`/applications/${position}-at-${company}?id=${id}`}
-      className={`bg-card-${status} group h-[5.75rem] rounded-md px-3 py-2 text-light-text`}
+      className={`bg-card-${status} group h-[5.75rem] rounded-md px-3 py-2 text-light-text dark:text-black`}
       data-testid="board-section-card"
       aria-label={`Open application for ${position} at ${company}`}
     >
