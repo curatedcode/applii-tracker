f01dbb34dad6bffd24eb281a605dd4d2634faeb2
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-10-20 08:58:58 -0400

    fix text on buttons wrapping

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({
   if (style === "icon") {
     return (
       <button
-        className={`h-fit w-fit rounded-md p-0.5 font-medium transition-opacity hover:opacity-80 ${className}`}
+        className={`h-fit w-fit whitespace-nowrap rounded-md p-0.5 font-medium transition-opacity hover:opacity-80 ${className}`}
         type={type}
         {...props}
       >
@@ -21,7 +21,7 @@ export default function Button({
 
   return (
     <button
-      className={`flex h-fit min-w-[6rem] items-center justify-center gap-1 rounded-md bg-light-secondary px-5 py-2 text-center text-sm font-medium outline-none transition-all duration-100 focus-within:outline-none focus:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-opacity-75 focus-visible:ring-offset-black dark:bg-dark-secondary dark:focus-visible:ring-light-secondary dark:focus-visible:ring-offset-light-secondary ${className}`}
+      className={`flex h-fit min-w-[6rem] items-center justify-center gap-1 whitespace-nowrap rounded-md bg-light-secondary px-5 py-2 text-center text-sm font-medium outline-none transition-all duration-100 focus-within:outline-none focus:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-opacity-75 focus-visible:ring-offset-black dark:bg-dark-secondary dark:focus-visible:ring-light-secondary dark:focus-visible:ring-offset-light-secondary ${className}`}
       type={type}
       {...props}
     >
