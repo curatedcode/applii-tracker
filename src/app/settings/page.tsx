fc453be3ffdab3c32580cfc28a81d2bb1a942567
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-10-21 07:46:30 -0400

    add button-like style for external link

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -77,7 +77,7 @@ export default function Settings() {
               <ExternalLink
                 href={dropboxAuthURL}
                 onClick={() => setShowTokenInput(true)}
-                className="flex h-fit min-w-[6rem] items-center justify-center gap-1 rounded-md bg-light-secondary px-5 py-2 text-center text-sm font-medium outline-none transition-all duration-100 focus-within:outline-none focus:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-opacity-75 focus-visible:ring-offset-black dark:bg-dark-secondary dark:focus-visible:ring-light-secondary dark:focus-visible:ring-offset-light-secondary"
+                style="button"
               >
                 Get access token
               </ExternalLink>
