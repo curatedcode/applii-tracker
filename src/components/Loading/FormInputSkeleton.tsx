6df33c16e65bb52afd4dbfe48b16a57cf78895f0
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-10-25 10:01:59 -0400

    restyle loading skeletons

--- src/components/Loading/FormInputSkeleton.tsx
+++ src/components/Loading/FormInputSkeleton.tsx
@@ -7,12 +7,12 @@ export default function FormInputSkeleton({
     return (
       <div className="grid">
         <div className="mb-1 h-6"></div>
-        <div className="h-10 w-full animate-pulse border-[0.4rem] border-light-secondary bg-light-secondary p-[0.4rem] dark:border-dark-secondary dark:bg-dark-secondary"></div>
+        <div className="h-10 w-full animate-pulse rounded-md border-[0.4rem] border-light-secondary bg-light-secondary p-[0.4rem] dark:border-dark-secondary dark:bg-dark-secondary"></div>
       </div>
     );
   }
 
   return (
-    <div className="h-10 w-full animate-pulse border-[0.4rem] border-light-secondary bg-light-secondary p-[0.4rem] dark:border-dark-secondary dark:bg-dark-secondary"></div>
+    <div className="h-10 w-full animate-pulse rounded-md border-[0.4rem] border-light-secondary bg-light-secondary p-[0.4rem] dark:border-dark-secondary dark:bg-dark-secondary"></div>
   );
 }
--- src/components/Loading/ViewApplicationSkeleton.tsx
+++ src/components/Loading/ViewApplicationSkeleton.tsx
@@ -4,7 +4,7 @@ export default function ViewApplicationSkeleton() {
       <div aria-live="polite" className="sr-only">
         <p>Loading application.</p>
       </div>
-      <div className="h-[100px]"></div>
+      <div className="h-20"></div>
       <div className="grid w-full justify-items-center gap-x-12 gap-y-20 justify-self-center md:max-w-5xl md:grid-cols-2">
         <div className="grid w-full max-w-md auto-rows-min justify-items-center md:max-w-none">
           <div className="mb-6 h-[70px] w-full"></div>
@@ -12,11 +12,11 @@ export default function ViewApplicationSkeleton() {
         </div>
         <div className="grid w-full max-w-md auto-rows-min justify-items-center md:max-w-none">
           <div className="mb-6 h-[70px] w-full"></div>
-          <div className="animate-delay-200 h-[304px] min-w-full max-w-full animate-pulse rounded-md bg-light-secondary dark:bg-dark-secondary"></div>
+          <div className="h-[304px] min-w-full max-w-full animate-pulse rounded-md bg-light-secondary dark:bg-dark-secondary"></div>
         </div>
         <div className="grid w-full max-w-md auto-rows-min justify-items-center justify-self-center md:col-span-full md:max-w-none">
           <div className="mb-6 h-[70px] w-full"></div>
-          <div className="animate-delay-400 h-[304px] w-full max-w-full animate-pulse rounded-md bg-light-secondary dark:bg-dark-secondary"></div>
+          <div className="h-[304px] w-full max-w-full animate-pulse rounded-md bg-light-secondary dark:bg-dark-secondary"></div>
         </div>
       </div>
     </>
