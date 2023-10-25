fa342b5587e9397181034c213db16677821f023c
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-10-25 10:11:14 -0400

    update styling of indexedDB not supported error

--- src/components/IndexedDBNotSupported.tsx
+++ src/components/IndexedDBNotSupported.tsx
@@ -1,11 +1,16 @@
+import { NoSymbolIcon } from "@heroicons/react/24/outline";
 import ExternalLink from "./Links/ExternalLink";
 
 export default function IndexedDBNotSupported() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <p role="error" className="w-full max-w-md">
-        Your browser is not supported. Please exit incognito or private mode.
-        Otherwise download a supported browser like{" "}
+    <div className="mt-24 grid w-full max-w-sm place-items-center gap-6 justify-self-center">
+      <div className="flex gap-2 text-lg font-semibold md:text-xl">
+        <NoSymbolIcon className="w-6" aria-hidden="true" />
+        <h1>Your browser is not supported</h1>
+      </div>
+      <p>
+        Please exit incognito or private mode. Otherwise, download a supported
+        browser like{" "}
         <ExternalLink
           href="https://www.mozilla.org/en-US/firefox/new/"
           className="underline"
@@ -19,6 +24,7 @@ export default function IndexedDBNotSupported() {
         >
           Google Chrome
         </ExternalLink>
+        .
       </p>
     </div>
   );
