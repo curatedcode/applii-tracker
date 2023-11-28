a55d3b72e426d38530317c638e8cbc0b5b3605ec
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-11-28 18:02:47 -0500

    update design of loading skeletons

--- src/components/Loading/MetricsSkeleton.tsx
+++ src/components/Loading/MetricsSkeleton.tsx
@@ -6,8 +6,8 @@ export default function MetricsSkeleton() {
       </div>
       <div className="flex w-full items-center justify-center">
         <div className="mt-24 grid w-full max-w-6xl justify-items-center gap-12 justify-self-center md:gap-20">
-          <div className="h-60 w-full max-w-xs animate-pulse rounded-md border-[0.4rem] border-light-secondary bg-light-secondary p-[0.4rem] dark:border-dark-secondary dark:bg-dark-secondary"></div>
-          <div className="h-80 w-full animate-pulse rounded-md border-[0.4rem] border-light-secondary bg-light-secondary p-[0.4rem] dark:border-dark-secondary dark:bg-dark-secondary"></div>
+          <div className="h-[36rem] w-full animate-pulse rounded-md border-[0.4rem] border-light-secondary bg-light-secondary p-[0.4rem] dark:border-dark-secondary dark:bg-dark-secondary"></div>
+          <div className="h-60 w-full max-w-sm animate-pulse rounded-md border-[0.4rem] border-light-secondary bg-light-secondary p-[0.4rem] dark:border-dark-secondary dark:bg-dark-secondary"></div>
         </div>
       </div>
     </>
