d3541fe1aad4820f163f4e77e9b7417a02d8cb7c
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-12-01 17:58:56 -0500

    add landing page

---
+++ src/components/Landing/BoardSectionCard.tsx
@@ -0,0 +1,30 @@
+import { FullApplicationType } from "@/src/utils/customVariables";
+import relativeDate from "../Fn/relativeDate";
+
+export default function BoardSectionCard({
+  position,
+  company,
+  dateCreated,
+  status,
+}: FullApplicationType) {
+  const date = relativeDate(dateCreated, "dateCreated");
+
+  return (
+    <div
+      className={`bg-card-${status} h-board-section-card rounded-md px-3 py-2 text-black`}
+    >
+      <div className="relative flex h-full flex-col justify-between">
+        <div className="grid font-medium">
+          <span className="line-clamp-1">{position}</span>
+          <span className="line-clamp-1">{company}</span>
+        </div>
+        <span
+          className="absolute bottom-0.5 right-0 text-sm"
+          title={date.title}
+        >
+          {date.time}
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Landing/StarryCanvas.tsx
@@ -0,0 +1,161 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import type { Engine } from "tsparticles-engine";
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+
+export default function StarryCanvas() {
+  const particlesId = "particlesBG";
+
+  const [prefersDarkScheme, setPrefersDarkScheme] = useState(false);
+
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  useEffect(() => {
+    const darkSchemeMediaMatch = window.matchMedia(
+      "(prefers-color-scheme: dark)",
+    );
+    setPrefersDarkScheme(darkSchemeMediaMatch.matches);
+
+    function updatePrefersDarkScheme(e: MediaQueryListEvent) {
+      setPrefersDarkScheme(e.matches);
+    }
+
+    darkSchemeMediaMatch.addEventListener("change", updatePrefersDarkScheme);
+
+    return () =>
+      darkSchemeMediaMatch.removeEventListener(
+        "change",
+        updatePrefersDarkScheme,
+      );
+  }, []);
+
+  // no way to reinitialize particles after creation
+
+  if (prefersDarkScheme) {
+    return (
+      <Particles
+        id={particlesId}
+        init={particlesInit}
+        style={{ position: "fixed" }}
+        className="pointer-events-none fixed z-[-1] h-full w-full"
+        options={{
+          detectRetina: true,
+          fpsLimit: 30,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              resize: true,
+            },
+          },
+          particles: {
+            color: {
+              value: "#fcfdfd",
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1080,
+              },
+              limit: 0,
+              value: 200,
+            },
+            opacity: {
+              animation: {
+                enable: true,
+                minimumValue: 0.05,
+                speed: 0.25,
+                sync: false,
+              },
+              random: {
+                enable: true,
+                minimumValue: 0.05,
+              },
+              value: 1,
+            },
+            move: {
+              direction: "bottom",
+              enable: true,
+              speed: 0.05,
+              straight: true,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: {
+                enable: true,
+                minimumValue: 0.5,
+              },
+              value: 1,
+            },
+          },
+        }}
+      />
+    );
+  }
+
+  return (
+    <Particles
+      id={particlesId}
+      init={particlesInit}
+      style={{ position: "fixed" }}
+      className="pointer-events-none fixed z-[-1] h-full w-full"
+      options={{
+        detectRetina: true,
+        fpsLimit: 30,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            resize: true,
+          },
+        },
+        particles: {
+          color: {
+            value: "#262626",
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 1080,
+            },
+            limit: 0,
+            value: 200,
+          },
+          opacity: {
+            animation: {
+              enable: true,
+              minimumValue: 0.05,
+              speed: 0.25,
+              sync: false,
+            },
+            random: {
+              enable: true,
+              minimumValue: 0.05,
+            },
+            value: 1,
+          },
+          move: {
+            direction: "bottom",
+            enable: true,
+            speed: 0.05,
+            straight: true,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: {
+              enable: true,
+              minimumValue: 0.5,
+            },
+            value: 1,
+          },
+        },
+      }}
+    />
+  );
+}
