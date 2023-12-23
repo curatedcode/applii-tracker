0bdcaa513d6e6d1e7ff23f3a32926a84c11c2a34
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-12-23 09:50:24 -0500

    add icon for loading spinner

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -1,5 +1,6 @@
 import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { Toaster as ToasterRenderer } from "react-hot-toast";
+import LoadingSpinnerIcon from "./LoadingSpinnerIcon";
 
 export default function Toaster() {
   return (
@@ -9,23 +10,7 @@ export default function Toaster() {
         duration: 5000,
         ariaProps: { "aria-live": "polite", role: "status" },
         loading: {
-          icon: (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              aria-hidden="true"
-              viewBox="0 0 24 24"
-              className="h-6 w-6 animate-spin text-dark-primary dark:text-light-primary"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1.5}
-                d="M12 4.75v1.5M17.127 6.873l-1.061 1.061M19.25 12h-1.5M17.127 17.127l-1.061-1.061M12 17.75v1.5M7.934 16.066l-1.06 1.06M6.25 12h-1.5M7.934 7.934l-1.06-1.06"
-              />
-            </svg>
-          ),
+          icon: <LoadingSpinnerIcon />,
         },
         error: {
           icon: (
