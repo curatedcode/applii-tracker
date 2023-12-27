c4fcbfcd1b48faded7ada8d4709a991c9004548c
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2023-12-27 14:48:16 -0500

    update settings skeleton

--- src/components/Loading/SettingsSkeleton.tsx
+++ src/components/Loading/SettingsSkeleton.tsx
@@ -4,7 +4,7 @@ export default function SettingsSkeleton() {
       <div aria-live="polite" className="sr-only">
         <p>Loading settings.</p>
       </div>
-      <div className="mt-[84px] h-52 w-full max-w-md animate-pulse justify-self-center rounded-md bg-light-secondary dark:bg-dark-secondary"></div>
+      <div className="mt-28 h-[704px] w-full max-w-lg animate-pulse justify-self-center rounded-md bg-light-secondary dark:bg-dark-secondary md:h-[630px]"></div>
     </>
   );
 }
