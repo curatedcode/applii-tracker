a87fcdcf6f662402c95a464732e418b98e9586da
Zackary Fotheringham <zackaryfotheringham@hotmail.com>
2024-01-30 15:46:55 -0500

    update board section card time format

--- tests/relativeDate.spec.ts
+++ tests/relativeDate.spec.ts
@@ -2,7 +2,7 @@ import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import relativeDate from "../src/components/Fn/relativeDate";
 
 describe("Relative time with created label", () => {
-	describe("seconds being returned as minutes", () => {
+	describe("seconds", () => {
 		beforeEach(() => {
 			vi.useFakeTimers();
 		});
@@ -11,39 +11,27 @@ describe("Relative time with created label", () => {
 			vi.useRealTimers();
 		});
 
-		it("should return 1 second ago as 1m", () => {
+		it("should return 0 second ago as now", () => {
 			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
 			vi.setSystemTime(fakeSystemDate);
 
-			const fakeNow = new Date(2023, 3, 30, 1, 0, 1).toISOString();
+			const fakeNow = new Date(2023, 3, 30, 1, 0, 0).toISOString();
 
 			expect(relativeDate(fakeNow, "dateCreated")).toStrictEqual({
-				time: "1m",
-				title: "Created 1m ago",
+				time: "Now",
+				title: "Created now",
 			});
 		});
 
-		it("should return 5 seconds ago as 1m", () => {
+		it("should return 5 seconds ago as 5s", () => {
 			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
 			vi.setSystemTime(fakeSystemDate);
 
 			const fakeNow = new Date(2023, 3, 30, 1, 0, 5).toISOString();
 
 			expect(relativeDate(fakeNow, "dateCreated")).toStrictEqual({
-				time: "1m",
-				title: "Created 1m ago",
-			});
-		});
-
-		it("should return 59 seconds ago as 1m", () => {
-			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
-			vi.setSystemTime(fakeSystemDate);
-
-			const fakeNow = new Date(2023, 3, 30, 1, 0, 59).toISOString();
-
-			expect(relativeDate(fakeNow, "dateCreated")).toStrictEqual({
-				time: "1m",
-				title: "Created 1m ago",
+				time: "5s",
+				title: "Created 5s ago",
 			});
 		});
 	});
@@ -57,27 +45,27 @@ describe("Relative time with created label", () => {
 			vi.useRealTimers();
 		});
 
-		it("should return 1m", () => {
+		it("should return 1min", () => {
 			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
 			vi.setSystemTime(fakeSystemDate);
 
 			const fakeNow = new Date(2023, 3, 30, 1, 1, 0).toISOString();
 
 			expect(relativeDate(fakeNow, "dateCreated")).toStrictEqual({
-				time: "1m",
-				title: "Created 1m ago",
+				time: "1min",
+				title: "Created 1min ago",
 			});
 		});
 
-		it("should return 5m", () => {
+		it("should return 5min", () => {
 			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
 			vi.setSystemTime(fakeSystemDate);
 
 			const fakeNow = new Date(2023, 3, 30, 1, 5).toISOString();
 
 			expect(relativeDate(fakeNow, "dateCreated")).toStrictEqual({
-				time: "5m",
-				title: "Created 5m ago",
+				time: "5min",
+				title: "Created 5min ago",
 			});
 		});
 	});
@@ -229,39 +217,27 @@ describe("Relative date with updated label", () => {
 			vi.useRealTimers();
 		});
 
-		it("should return 1 second ago as 1m", () => {
+		it("should return 0 second ago as now", () => {
 			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
 			vi.setSystemTime(fakeSystemDate);
 
-			const fakeNow = new Date(2023, 3, 30, 1, 0, 1).toISOString();
+			const fakeNow = new Date(2023, 3, 30, 1, 0, 0).toISOString();
 
-			expect(relativeDate(fakeNow, "dateModified")).toStrictEqual({
-				time: "1m",
-				title: "Updated 1m ago",
+			expect(relativeDate(fakeNow, "dateCreated")).toStrictEqual({
+				time: "Now",
+				title: "Created now",
 			});
 		});
 
-		it("should return 5 seconds ago as 1m", () => {
+		it("should return 5 seconds ago as 5s", () => {
 			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
 			vi.setSystemTime(fakeSystemDate);
 
 			const fakeNow = new Date(2023, 3, 30, 1, 0, 5).toISOString();
 
-			expect(relativeDate(fakeNow, "dateModified")).toStrictEqual({
-				time: "1m",
-				title: "Updated 1m ago",
-			});
-		});
-
-		it("should return 59 seconds ago as 1m", () => {
-			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
-			vi.setSystemTime(fakeSystemDate);
-
-			const fakeNow = new Date(2023, 3, 30, 1, 0, 59).toISOString();
-
-			expect(relativeDate(fakeNow, "dateModified")).toStrictEqual({
-				time: "1m",
-				title: "Updated 1m ago",
+			expect(relativeDate(fakeNow, "dateCreated")).toStrictEqual({
+				time: "5s",
+				title: "Created 5s ago",
 			});
 		});
 	});
@@ -275,27 +251,27 @@ describe("Relative date with updated label", () => {
 			vi.useRealTimers();
 		});
 
-		it("should return 1m", () => {
+		it("should return 1min", () => {
 			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
 			vi.setSystemTime(fakeSystemDate);
 
 			const fakeNow = new Date(2023, 3, 30, 1, 1, 0).toISOString();
 
 			expect(relativeDate(fakeNow, "dateModified")).toStrictEqual({
-				time: "1m",
-				title: "Updated 1m ago",
+				time: "1min",
+				title: "Updated 1min ago",
 			});
 		});
 
-		it("should return 5m", () => {
+		it("should return 5min", () => {
 			const fakeSystemDate = new Date(2023, 3, 30, 1, 0, 0);
 			vi.setSystemTime(fakeSystemDate);
 
 			const fakeNow = new Date(2023, 3, 30, 1, 5).toISOString();
 
 			expect(relativeDate(fakeNow, "dateModified")).toStrictEqual({
-				time: "5m",
-				title: "Updated 5m ago",
+				time: "5min",
+				title: "Updated 5min ago",
 			});
 		});
 	});
